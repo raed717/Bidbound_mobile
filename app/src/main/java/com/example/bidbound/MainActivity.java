dddb5c30e6d39ba1bef1de84ea54a76499483fd9
firasyazid <98762038+firasyazid@users.noreply.github.com>
2023-11-11 16:44:03 +0100

    push

--- app/src/main/java/com/example/bidbound/MainActivity.java
+++ app/src/main/java/com/example/bidbound/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.bidbound;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup=findViewById(R.id.SignUpTextView);
+        signup.setOnClickListener(e -> {
+
+            Intent intent = new Intent(this, SignUpActivity.class);
+            startActivity(intent);
+
+        });
     }
+
+
+    public void openSignUpActivity(View view) {
+    }
+
+
 }
