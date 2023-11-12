ad6facac7337a0a0e7d6f3012a1768a715cb79bb
firasyazid <98762038+firasyazid@users.noreply.github.com>
2023-11-12 02:52:59 +0100

    PUSH

--- app/src/main/java/com/example/bidbound/MainActivity.java
+++ app/src/main/java/com/example/bidbound/MainActivity.java
@@ -2,28 +2,41 @@ package com.example.bidbound;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 TextView signup;
+SharedPreferences myPref ;
+public static final String Pref = "MyPref";
+EditText login , password ;
+Button save ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
         signup=findViewById(R.id.SignUpTextView);
         signup.setOnClickListener(e -> {
-
             Intent intent = new Intent(this, SignUpActivity.class);
             startActivity(intent);
-
         });
     }
 
 
-    public void openSignUpActivity(View view) {
-    }
+
 
 
 }
--- app/src/main/java/com/example/bidbound/SignUpActivity.java
+++ app/src/main/java/com/example/bidbound/SignUpActivity.java
@@ -1,16 +1,48 @@
 package com.example.bidbound;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
+    SharedPreferences myPref ;
+    public static final String Pref = "MyPref";
+    EditText name , mail,  pass ;
+    Button register ;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        name=findViewById(R.id.username);
+        mail=findViewById(R.id.email);
+        pass=findViewById(R.id.password);
+
+        register=findViewById(R.id.signupbtn);
+
+        myPref = getSharedPreferences(Pref,MODE_PRIVATE);
+        SharedPreferences.Editor editor=myPref.edit();
+
+
+
+        register.setOnClickListener(e -> {
+            editor.putString("name", name.getText().toString());
+            editor.putString("mail", mail.getText().toString());
+            editor.putString("pwd", pass.getText().toString());
+            editor.apply();
+
+
+
+
+        });
+
+
     }
 
 }
