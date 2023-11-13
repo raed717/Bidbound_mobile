f93ae95bf90d2215d03735e92cdc275ba7a8108e
mensia <hana.mensia@esprit.tn>
2023-11-13 14:54:02 +0100

    add tasks

---
+++ app/src/main/java/com/example/bidbound/Taches/TacheMainActivity.java
@@ -0,0 +1,18 @@
+package com.example.bidbound.Taches;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bidbound.R;
+
+public class TacheMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tache_activity_main);
+    }
+
+
+}
