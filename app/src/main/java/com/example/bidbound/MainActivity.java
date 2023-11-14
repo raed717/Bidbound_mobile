772d24bb6970684b6c72c958bcb581e615616a31
mensia <hana.mensia@esprit.tn>
2023-11-14 10:32:09 +0100

    nour + hana

--- app/src/main/java/com/example/bidbound/MainActivity.java
+++ app/src/main/java/com/example/bidbound/MainActivity.java
@@ -8,16 +8,20 @@ import android.view.View;
 import android.widget.Button;
 
 //import com.example.bidbound.Projects.ProjectMenuActivity;
+import com.example.bidbound.Membres.MemberActivity;
 import com.example.bidbound.Taches.TacheMainActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private Button TaskButton;private Button ProjectButton;
+    private Button TaskButton;
+    private Button ProjectButton;
+    private Button MemberButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TaskButton = (Button) findViewById(R.id.Tasks);
         ProjectButton = (Button) findViewById(R.id.Projects);
+        MemberButton = (Button) findViewById(R.id.Member);
         TaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,10 +34,20 @@ public class MainActivity extends AppCompatActivity {
                 openProjects();
             }
         });
+        MemberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMember();
+            }
+        });
     }
 
     public void openTasks(){
         Intent intent = new Intent(this, TacheMainActivity.class);
+        startActivity(intent);
+    }
+    public void openMember(){
+        Intent intent = new Intent(this, MemberActivity.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/bidbound/Taches/Fragments/AfficherListTachesFragment.java
+++ app/src/main/java/com/example/bidbound/Taches/Fragments/AfficherListTachesFragment.java
@@ -2,7 +2,9 @@ package com.example.bidbound.Taches.Fragments;
 
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -25,6 +27,7 @@ import android.view.View;
 
 import android.widget.Toast;
 
+import com.example.bidbound.MainActivity;
 import com.example.bidbound.R;
 
 
@@ -33,6 +36,8 @@ public class AfficherListTachesFragment extends AppCompatActivity {
     private ImageButton taskDetails,AddTaskB, searchButtonTasks;
     private TextView tasksDataView;
     private EditText searchEditText;
+    private Button HomeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +48,19 @@ public class AfficherListTachesFragment extends AppCompatActivity {
         taskDetails = (ImageButton) findViewById(R.id.taskDetails);
         searchEditText = findViewById(R.id.searchEditText);
         tasksDataView = findViewById(R.id.tasksDataView);
+        HomeButton = findViewById(R.id.HomeTask) ;
 
 
         searchButtonTasks.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                searchMember(searchEditText.getText().toString());
+                searchTask(searchEditText.getText().toString());
+            }
+        });
+        HomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHome();
             }
         });
         taskDetails.setOnClickListener(new View.OnClickListener() {
@@ -60,7 +72,7 @@ public class AfficherListTachesFragment extends AppCompatActivity {
         AddTaskB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openAddMember();
+                openAddTask();
             }
         });
 
@@ -80,7 +92,7 @@ public class AfficherListTachesFragment extends AppCompatActivity {
         Intent intent = new Intent(this, com.example.bidbound.Taches.Fragments.DetailsTacheFragment.class);
         startActivity(intent);
     }
-    public void openAddMember(){
+    public void openAddTask(){
         Intent intent = new Intent(this, AjouterTache.class);
         startActivity(intent);
     }
@@ -104,10 +116,10 @@ public class AfficherListTachesFragment extends AppCompatActivity {
         }
 
         StringBuilder formattedData = new StringBuilder();
-        String[] members = rawData.split("\n");
+        String[] tasks = rawData.split("\n");
 
-        for (String member : members) {
-            String[] taskDetails = member.split(",");
+        for (String task : tasks) {
+            String[] taskDetails = task.split(",");
             if (taskDetails.length >= 3) { // Ensure there are enough fields
                 String taskName = taskDetails[0];
                 String Description = taskDetails[1];
@@ -121,16 +133,16 @@ public class AfficherListTachesFragment extends AppCompatActivity {
         return formattedData.toString();
     }
 
-    private void searchMember(String query) {
+    private void searchTask(String query) {
         try {
-            FileInputStream fis = openFileInput("member_data.txt");
+            FileInputStream fis = openFileInput("Task_data.txt");
             InputStreamReader isr = new InputStreamReader(fis);
             BufferedReader br = new BufferedReader(isr);
             String line;
             boolean found = false;
             while ((line = br.readLine()) != null) {
-                String[] memberDetails = line.split(",");
-                if (memberDetails[0].equalsIgnoreCase(query) || memberDetails[1].equalsIgnoreCase(query)) {
+                String[] taskDetails = line.split(",");
+                if (taskDetails[0].equalsIgnoreCase(query) || taskDetails[1].equalsIgnoreCase(query)) {
                     // Match found
                     found = true;
                     showSearchResult(formatData(line));
@@ -145,6 +157,10 @@ public class AfficherListTachesFragment extends AppCompatActivity {
             e.printStackTrace();
             Toast.makeText(this, "Error occurred during search", Toast.LENGTH_SHORT).show();
         }
+    }
+    public void openHome(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     private void showNoSearchResult(String query) {
@@ -163,17 +179,17 @@ public class AfficherListTachesFragment extends AppCompatActivity {
 
     private void showSearchResult(String message) {
         String[] lines = message.split("\n");
-        String memberFirstName = "";
+        String taskFirstName = "";
         for (String line : lines) {
-            if (line.startsWith("Member FirstName")) {
+            if (line.startsWith("Task FirstName")) {
                 String[] parts = line.split(":");
                 if (parts.length > 1) {
-                    memberFirstName = parts[1].trim();
+                    taskFirstName = parts[1].trim();
                 }
                 break;
             }
         }
-        String finalmemberFirstName = memberFirstName;
+        String finaltaskFirstName = taskFirstName;
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Search Result");
         builder.setMessage(message);
@@ -186,7 +202,7 @@ public class AfficherListTachesFragment extends AppCompatActivity {
 
         builder.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                deleteMember(finalmemberFirstName);
+                deleteTask(finaltaskFirstName);
             }
         });
 
@@ -194,10 +210,10 @@ public class AfficherListTachesFragment extends AppCompatActivity {
         dialog.show();
     }
 
-    private void deleteMember(String MemberNameToDelete) {
+    private void deleteTask(String TaskNameToDelete) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Search Result");
-        String message = "Are you sure that you want to delete  "+MemberNameToDelete+"?";
+        String message = "Are you sure that you want to delete  "+TaskNameToDelete+"?";
         builder.setMessage(message);
         builder.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
@@ -206,7 +222,7 @@ public class AfficherListTachesFragment extends AppCompatActivity {
         });
         builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                ConfirmDeleteMember(MemberNameToDelete);
+                ConfirmDeleteTask(TaskNameToDelete);
             }
         });
         AlertDialog dialog = builder.create();
@@ -214,9 +230,9 @@ public class AfficherListTachesFragment extends AppCompatActivity {
     }
 
 
-    private void ConfirmDeleteMember(String memberNameToDelete) {
+    private void ConfirmDeleteTask(String taskNameToDelete) {
         try {
-            FileInputStream fis = openFileInput("member_data.txt");
+            FileInputStream fis = openFileInput("Task_data.txt");
             byte[] buffer = new byte[1024];
             int bytesRead = fis.read(buffer);
             fis.close();
@@ -224,30 +240,30 @@ public class AfficherListTachesFragment extends AppCompatActivity {
             if (bytesRead > 0) {
                 String existingData = new String(buffer, 0, bytesRead);
 
-                String[] members = existingData.split("\n");
+                String[] tasks = existingData.split("\n");
                 StringBuilder updatedData = new StringBuilder();
 
-                boolean memberDeleted = false;
+                boolean taskDeleted = false;
 
-                for (String member : members) {
-                    String[] memberDetails = member.split(",");
-                    if (memberDetails.length >= 1 && memberDetails[0] != null
-                            && !memberDetails[0].equals(memberNameToDelete)) {
-                        updatedData.append(member).append("\n");
+                for (String task : tasks) {
+                    String[] taskDetails = task.split(",");
+                    if (taskDetails.length >= 1 && taskDetails[0] != null
+                            && !taskDetails[0].equals(taskNameToDelete)) {
+                        updatedData.append(task).append("\n");
                     } else {
                         // Project found and deleted
-                        memberDeleted = true;
+                        taskDeleted = true;
                     }
                 }
 
-                if (!memberDeleted) {
-                    Toast.makeText(this, "member not found", Toast.LENGTH_SHORT).show();
+                if (!taskDeleted) {
+                    Toast.makeText(this, "task not found", Toast.LENGTH_SHORT).show();
                 } else {
-                    FileOutputStream fos = openFileOutput("member_data.txt", Context.MODE_PRIVATE);
+                    FileOutputStream fos = openFileOutput("Task_data.txt", Context.MODE_PRIVATE);
                     fos.write(updatedData.toString().getBytes());
                     fos.close();
 
-                    Toast.makeText(this, "member deleted successfully", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "task deleted successfully", Toast.LENGTH_SHORT).show();
 
                     // Update your UI components if needed
                     String updatedDataString = readDataFromFile();
